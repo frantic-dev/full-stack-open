dd66ebb13b688b95f51031b19202c1a0ef033c9d
frantic-dev <107622593+frantic-dev@users.noreply.github.com>
2023-07-05 21:51:44 +0100

    course info step 5 finished

--- part_1/courseinfo/src/components/Total.jsx
+++ part_1/courseinfo/src/components/Total.jsx
@@ -1,5 +1,8 @@
 const Total = (props) => {
-  let total = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises;
+  let total =
+    props.parts[0].exercises +
+    props.parts[1].exercises +
+    props.parts[2].exercises;
   return <div>Number of exercises {total} </div>;
 };
 export default Total;
