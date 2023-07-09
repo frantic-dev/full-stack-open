c2fec1d361ae19af34f070fb3f2c273322b6a8ec
frantic-dev <107622593+frantic-dev@users.noreply.github.com>
2023-07-09 20:28:39 +0100

    part 1 unicafe step 4 finished

--- part_1/unicafe/src/App.jsx
+++ part_1/unicafe/src/App.jsx
@@ -1,17 +1,19 @@
 import { useState } from "react";
 
 const Statistics = (props) => {
-  return (
-    <div>
-      <h1>statistics</h1>
-      <p>good {props.good}</p>
-      <p>neutral {props.neutral}</p>
-      <p>bad {props.bad}</p>
-      <p>all {props.total}</p>
-      <p>average {props.average}</p>
-      <p>positive {props.positive}%</p>
-    </div>
-  );
+  if (props.total === 0) return <p>No feedback given</p>;
+  else
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>good {props.good}</p>
+        <p>neutral {props.neutral}</p>
+        <p>bad {props.bad}</p>
+        <p>all {props.total}</p>
+        <p>average {props.average}</p>
+        <p>positive {props.positive}%</p>
+      </div>
+    );
 };
 
 const App = () => {
@@ -29,9 +31,17 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-      <Statistics good={good} neutral={neutral} bad={bad} total ={total} average={average} positive={positive} />
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={total}
+        average={average}
+        positive={positive}
+      />
     </div>
   );
 };
 
 export default App;
+
