8e74f22fd38e635cf56ec7a02281a1abc249c4d7
frantic-dev <107622593+frantic-dev@users.noreply.github.com>
2023-07-09 20:40:14 +0100

    part 1 unicafe step 5 finished

--- part_1/unicafe/src/App.jsx
+++ part_1/unicafe/src/App.jsx
@@ -1,17 +1,28 @@
 import { useState } from "react";
 
+const Button = (props) => {
+  return <button onClick={props.handleClick}>{props.text} </button>;
+};
+
+const StatisticLine = (props) => {
+  return (
+    <p>
+      {props.text} {props.value}
+    </p>
+  );
+};
 const Statistics = (props) => {
   if (props.total === 0) return <p>No feedback given</p>;
   else
     return (
       <div>
         <h1>statistics</h1>
-        <p>good {props.good}</p>
-        <p>neutral {props.neutral}</p>
-        <p>bad {props.bad}</p>
-        <p>all {props.total}</p>
-        <p>average {props.average}</p>
-        <p>positive {props.positive}%</p>
+        <StatisticLine text="good" value={props.good} />
+        <StatisticLine text="neutral" value={props.neutral} />
+        <StatisticLine text="bad" value={props.bad} />
+        <StatisticLine text="total" value={props.total} />
+        <StatisticLine text="average" value={props.average} />
+        <StatisticLine text="positive" value={props.positive} />
       </div>
     );
 };
@@ -23,14 +34,14 @@ const App = () => {
   const [bad, setBad] = useState(0);
   const total = good + neutral + bad;
   const average = (good - bad) / total || 0;
-  const positive = (good * 100) / total || 0;
+  const positive = (good * 100) / total + "%" || 0;
 
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button text="good" handleClick={() => setGood(good + 1)} />
+      <Button text="neutral" handleClick={() => setNeutral(neutral + 1)} />
+      <Button text="bad" handleClick={() => setBad(bad + 1)} />
       <Statistics
         good={good}
         neutral={neutral}
