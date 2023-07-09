64404de5b424a8c63346edee83601e80654ac099
frantic-dev <107622593+frantic-dev@users.noreply.github.com>
2023-07-09 20:26:35 +0100

    part 1 unicafe step 3 finished

--- part_1/unicafe/src/App.jsx
+++ part_1/unicafe/src/App.jsx
@@ -1,29 +1,37 @@
-import { useState } from 'react'
+import { useState } from "react";
+
+const Statistics = (props) => {
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+      <p>all {props.total}</p>
+      <p>average {props.average}</p>
+      <p>positive {props.positive}%</p>
+    </div>
+  );
+};
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const total = good + neutral + bad
-  const average = (good - bad) / total || 0
-  const positive = good * 100 / total || 0
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const total = good + neutral + bad;
+  const average = (good - bad) / total || 0;
+  const positive = (good * 100) / total || 0;
 
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={()=>setGood(good + 1)} >good</button>
-      <button onClick={()=>setNeutral(neutral + 1)} >neutral</button>
-      <button onClick={()=>setBad(bad + 1)} >bad</button>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {average}</p>
-      <p>positive {positive}%</p>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Statistics good={good} neutral={neutral} bad={bad} total ={total} average={average} positive={positive} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
