e6b15fce7384c85c9561c329b8f0e5eb85e1926a
frantic-dev <107622593+frantic-dev@users.noreply.github.com>
2023-07-19 19:13:12 +0100

    2.9 the phonebook step 4 finished

--- part_2/phonebook/src/App.jsx
+++ part_2/phonebook/src/App.jsx
@@ -2,16 +2,26 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "39-44-5323523" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
+  const filtered = persons.filter((person) =>
+    person.name.toLowerCase().match(filter)
+  );
+  const numbersToShow = filter !== "" ? filtered : persons;
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const names = persons.map((person) => person.name);
     if (!names.includes(newName))
-      setPersons((persons) => persons.concat({ name: newName, number: newNumber }));
+      setPersons((persons) =>
+        persons.concat({ name: newName, number: newNumber })
+      );
     else alert(`${newName} is already added to phonebook`);
     setNewName("");
     setNewNumber("");
@@ -20,6 +30,16 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        <label htmlFor="filter">filter shown with</label>
+        <input
+          type="search"
+          id="filter"
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
+        />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name:
@@ -38,9 +58,9 @@ const App = () => {
         </div>
       </form>
       <h2> Numbers</h2>
-      {persons.map((person) => (
+      {numbersToShow.map((person) => (
         <div key={person.name}>
-          {person.name} {person.number}{" "}
+          {person.name} {person.number}
         </div>
       ))}
     </div>
