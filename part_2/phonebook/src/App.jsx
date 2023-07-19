c8e579b90a80296eb415e1d23055534bbab2c59a
frantic-dev <107622593+frantic-dev@users.noreply.github.com>
2023-07-19 18:35:35 +0100

    2.8 the phonebook step 3 finished

--- part_2/phonebook/src/App.jsx
+++ part_2/phonebook/src/App.jsx
@@ -1,16 +1,20 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "39-44-5323523" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const names = persons.map((person) => person.name);
     if (!names.includes(newName))
-      setPersons((persons) => persons.concat({ name: newName }));
-    else alert(`${newName} is already added to phonebook`)
+      setPersons((persons) => persons.concat({ name: newName, number: newNumber }));
+    else alert(`${newName} is already added to phonebook`);
     setNewName("");
+    setNewNumber("");
   };
 
   return (
@@ -18,8 +22,16 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name:{" "}
+          name:
           <input value={newName} onChange={(e) => setNewName(e.target.value)} />
+        </div>
+        <div>
+          number:{" "}
+          <input
+            type="text"
+            value={newNumber}
+            onChange={(e) => setNewNumber(e.target.value)}
+          />
         </div>
         <div>
           <button type="submit">add</button>
@@ -27,7 +39,9 @@ const App = () => {
       </form>
       <h2> Numbers</h2>
       {persons.map((person) => (
-        <div key={person.name}>{person.name} </div>
+        <div key={person.name}>
+          {person.name} {person.number}{" "}
+        </div>
       ))}
     </div>
   );
